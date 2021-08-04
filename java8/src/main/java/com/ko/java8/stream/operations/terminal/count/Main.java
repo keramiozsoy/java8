435fbaed18ae79cbeef1ee0e2a146981fbb1cda3
kerami <keramiozsoy@hotmail.com>
2021-08-04 17:28:02 +0300

    count

---
+++ java8/src/main/java/com/ko/java8/stream/operations/terminal/count/Main.java
@@ -0,0 +1,17 @@
+package com.ko.java8.stream.operations.terminal.count;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        List<Integer> listOfInteger = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9);
+
+        final long result = listOfInteger.stream()
+                .count();
+
+        System.out.println(result);
+
+    }
+}
