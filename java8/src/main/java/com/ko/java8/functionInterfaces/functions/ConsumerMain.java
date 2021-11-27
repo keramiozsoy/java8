d0dc41cf5d3e4a841a271f3b87c50a8c1174a717
kozsoysolviads <kerami.ozsoy@solviads.com>
2021-11-27 17:30:22 +0300

    xml url

--- java8/src/main/java/com/ko/java8/functionInterfaces/functions/ConsumerMain.java
+++ java8/src/main/java/com/ko/java8/functionInterfaces/functions/ConsumerMain.java
@@ -1,13 +1,22 @@
 package com.ko.java8.functionInterfaces.functions;
 
 import java.util.function.Consumer;
+import java.util.function.Supplier;
 import java.util.stream.Stream;
 
 public class ConsumerMain {
     public static void main(String[] args) {
+// First Example
         Consumer<String> consumer = s -> System.out.println(s.toUpperCase());
 
         Stream<String> alphabet = Stream.of("a", "b", "c", "d");
         alphabet.forEach(consumer);
+
+// Second Example
+//        Consumer<String> consumer = s -> System.out.println(s.toUpperCase());
+//        Supplier<String> supplier = () -> "abcd";
+//
+//        Stream<String> alphabet = Stream.generate(supplier);
+//        alphabet.forEach(consumer);
     }
 }
